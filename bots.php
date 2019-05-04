7ad1049e906ebd883f4e9bb0dd222784b81cff0d
Wiedy Mi <wiedymi0@gmail.com>
2019-05-04 16:36:30 +0300

    hello

---
+++ bots.php
@@ -0,0 +1,101 @@
+<?php
+ini_set('max_execution_time', 300);
+
+
+require("vendor/autoload.php");
+
+$instagram = new \Instagram\Instagram();
+
+function GetMediaId($url) {
+ 
+    $user = curl_init("https://api.instagram.com/oembed?callback=&url=" . $url);
+        
+    curl_setopt($user, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($user, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($user, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($user, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($user, CURLOPT_HTTPHEADER, array(
+        'x-ig-capabilities' =>'3w==',
+        'user-agent'=> 'Instagram 9.5.1 (iPhone9,2; iOS 10_0_2; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
+  
+        
+    ));
+    
+    $users = curl_exec($user);
+    curl_close($user);
+    $data = json_decode($users, true);
+
+    return $data["media_id"];
+}
+function GetUserId($url)
+{
+    $username = parse_url($url, PHP_URL_PATH);
+    $username = str_replace('/', '',$username);
+   
+
+    $user = curl_init("https://www.instagram.com/web/search/topsearch/?query=" . $username);
+        
+    curl_setopt($user, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($user, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($user, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($user, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($user, CURLOPT_HTTPHEADER, array(
+        'x-ig-capabilities' =>'3w==',
+        'user-agent'=> 'Instagram 9.5.1 (iPhone9,2; iOS 10_0_2; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
+  
+        
+    ));
+    
+    $users = curl_exec($user);
+    curl_close($user);
+    $data = json_decode($users, true);
+
+    return $data["users"][0]["user"]["pk"];
+}
+print_r($_POST);
+if(!$_POST){
+    echo "error";
+    die();  
+}
+
+$accounts = "accounts.txt";
+$accounts = file($accounts);//file in to an array
+
+
+foreach($accounts as $line) 
+{
+    $var = explode(' ', $line, 3);
+    $users[] = [
+        "username" => $var[0],
+        "email" => $var[1],
+        "password" => $var[2],
+    ];
+}
+
+
+if($_POST["type" === "likes"]) if ($tmp++ < $_POST["num-likes"]){
+    echo "1";
+    foreach($users as $k => $v)
+    {
+        $instagram->login($v["username"], $v["password"]);
+        $media = $_POST["post-url"]; //Input your url
+        
+
+        $instagram->likeMedia(GetMediaId($media));
+        $instagram->logout();
+    }
+}
+
+
+if($_POST["type" === "followers"]){
+    echo "2";
+    foreach($users as $k => $v) if ($tmp++ < $_POST["num-foll"])
+    {
+        $instagram->login($v["username"], $v["password"]);
+        $user = $_POST["pro-url"]; //Input your url
+        
+        $instagram->followUser(GetUserId($user));
+        
+        $instagram->logout();
+    }
+}
